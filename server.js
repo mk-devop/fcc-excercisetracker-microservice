7ff022a870fc5dcce47bd5a633e72d6469ceb98a
mk-devop <101245598+mk-devop@users.noreply.github.com>
2022-03-10 14:31:17 +0300

    update

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 /* MONGO OPERATIONS*/
 const DB_URI = process.env.DB_URI;
 mongoose.connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const Schema = mongoose.Schema;
 
@@ -24,12 +24,15 @@ const exerciseSchema = new Schema({
   username: String,
   description: String,
   duration: Number,
-  date: Date,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 const userSchema = new Schema({
-  username: String
+  username: String,
 });
 const User = mongoose.model("User", userSchema);
 
@@ -38,65 +41,85 @@ const User = mongoose.model("User", userSchema);
 /* API OPERATIONS */
 
 // Add user
-app.post('/api/users', async function(req, res) {
-  const  userName = req.body.username;
-  var user = await User.findOne({username: userName});
-  if(user){
-    res.json({"username":user.username, "_id": user._id});
+app.post("/api/users", async function (req, res) {
+  const userName = req.body.username;
+  var user = await User.findOne({ username: userName });
+  if (user) {
+    res.json({ username: user.username, _id: user._id });
     return;
   }
-  
-  user = new User({ 
-      username: userName
+
+  user = new User({
+    username: userName,
   });
   const u = await user.save();
-  res.json({"username":u.username,"_id":u._id});
-
+  res.json({ username: u.username, _id: u._id });
 });
 
 // Add exercise
-app.post('/api/users/:_id/exercises', async function(req, res) {
-  const  id = req.params._id;
+app.post("/api/users/:_id/exercises", async function (req, res) {
+  const id = req.params._id;
   var { description, duration, date } = req.body;
-  
-  if(!date){
-    date = new Date().toISOString().slice(0, 10);
-  }
-  
-  var user = await User.findById(id);
-  // if(!user)
 
-  var exercise = new Exercise({ 
-    username : user.username,
+  var user = await User.findById(id);
+  if (!user) {
+    return res.json({ status: 400, message: "Unknown userId" });
+  }
+
+  var exercise = new Exercise({
+    username: user.username,
     description,
     duration,
-    date
+    date,
   });
   const ex = await exercise.save();
-  
-  let result = {"_id":user._id, "username":user.username,"date":ex.date.toDateString(), "duration":ex.duration, "description":ex.description};
-  
+
+  let result = {
+    username: user.username,
+    description: ex.description,
+    duration: ex.duration,
+    date: ex.date.toDateString(),
+    _id: user._id,
+  };
+
   res.json(result);
 });
 
 // Get Users
-app.get('/api/users', async function(req, res) {
+app.get("/api/users", async function (req, res) {
   let users = await User.find({});
-  let usersArry = users.map((m) => ({"username":m.username,"_id":m._id}));
+  let usersArry = users.map((m) => ({ username: m.username, _id: m._id }));
   res.json(usersArry);
 });
 
 // Get User Logs
-app.get('/api/users/:_id/logs', async function(req, res) {
-  const  id = req.params._id;
+app.get("/api/users/:_id/logs", async function (req, res) {
+  const id = req.params._id;
   //?[from][&to][&limit]
   let user = await User.findById(id);
-  let exercises = await Exercise.find({username : user.username})
-  //, date:{$gte: req.query.from, $lte: req.query.to}})
-  .limit(+req.query.limit);
-  
-  let logStr = exercises.map(m => ({"description":m.description,"duration":m.duration,"date":m.date.toDateString()}));
-  let result = {"_id":user._id, "username":user.username, "count":exercises.length, "log":logStr }
+  if (!user) {
+    return res.json({ status: 400, message: "Unknown userId" });
+  }
+
+  let exercises = await Exercise.find({ username: user.username })
+    //date:{$gte: req.query.from, $lte: req.query.to}
+    // date: {
+    //      $lte: hasTo ? to.toISOString() : Date.now(),
+    //      $gte: hasFrom ? from.toISOString() : 0
+    //    }
+    .limit(+req.query.limit);
+
+  let logStr = exercises.map((m) => ({
+    description: m.description,
+    duration: m.duration,
+    date: m.date.toDateString(),
+  }));
+  let result = {
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log: logStr,
+  };
 
   res.json(result);
 });
@@ -104,5 +127,5 @@ app.get('/api/users/:_id/logs', async function(req, res) {
 /* API OPERATIONS */
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
